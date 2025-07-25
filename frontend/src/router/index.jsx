6f7963d1b6abc5b659aa443d8d9f3a0fcd9ccc52
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-07-25 10:47:56 +0530

    Update

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
-import Dashboard from "../pages/admin/Dashboard";
-import Layout from "../layouts/Layout";
+// import Dashboard from "../pages/admin/Dashboard";
+// import Layout from "../layouts/Layout";
 
 const AppRouter = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/login" element={<Login />} />
-            <Route element={<Layout />}>
+
+            {/* <Route element={<Layout />}>
                 <Route path="/admin/dashboard" element={<Dashboard />} />
-            </Route>
+            </Route> */}
         </Routes>
     </BrowserRouter>
 );
