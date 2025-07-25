01877f1c297e7d422bdd179d465535ad8df21ad0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-07-25 19:24:10 +0530

    Update

--- frontend/src/components/Buttons/DefultBtn.jsx
+++ frontend/src/components/Buttons/DefultBtn.jsx
@@ -6,7 +6,7 @@ const DefaultBtn = ({ label = "Click the Button", onClick, type = "button", disa
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={`mt-6 px-6 py-2 rounded-md transition text-white font-medium
+            className={`mt-0 px-6 py-2 rounded-md transition text-white font-medium
                 ${disabled
                     ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
                     : 'bg-green-700 hover:bg-green-800'}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import useForm from '../hooks/useForm';
+import useAuth from '../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
+import DefaultInput from '../components/Form/DefaultInput';
+import DefaultBtn from '../components/Buttons/DefultBtn';
 
 const Login = () => {
-    return (
-        <div>
-            <div className="text-green-700 font-bold text-xl">Logn system</div>
-        </div>
-    )
-}
+    const { values, handleChange } = useForm({ email: '', password: '' });
+    const { setuser } = useAuth();
+    const navigate = useNavigate();
 
-export default Login
+    return (
+        <div className="flex justify-center items-center py-16 px-4">
+            <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8 space-y-6">
+                <h2 className="text-3xl font-semibold text-center text-gray-700">Welcome back to Hotel</h2>
+                <form action="" method="post" className="space-y-4">
+                    <DefaultInput
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        required
+                        placeholder={"Email Address"}
+                    />
+                    <DefaultInput
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        required
+                        placeholder={"Password"}
+                    />
+                    <div className="">
+                        <DefaultBtn type="submit" label="Login" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
