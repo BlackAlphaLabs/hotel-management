e74ea94e09b6b53dcda253905228eb86330219e1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-07-25 19:31:12 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,17 +3,35 @@ import useForm from '../hooks/useForm'
 import { useNavigate } from 'react-router-dom'
 import DefaultBtn from '../components/Buttons/DefultBtn'
 import DefaultInput from '../components/Form/DefaultInput'
+import API from '../services/api'
 
 
 const Register = () => {
     const { values, handleChange } = useForm({ username: '', email: '', password: '' })
     const navigate = useNavigate()
 
+    const headlesubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await API.post('/auth/register', values)
+            if (res.data.success === true) {
+                alert(res.data.Message)
+                navigate('/login')
+            }
+            else{
+                alert(res.data.Error)
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div className="flex justify-center items-center py-16 px-4">
             <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-8 space-y-6">
                 <h2 className="text-3xl font-semibold text-center text-gray-700">Welcome back to Hotel</h2>
-                <form action="" method="post" className="space-y-4">
+                <form onSubmit={headlesubmit} method="post" className="space-y-4">
                     <DefaultInput
                         type="text"
                         name="username"
